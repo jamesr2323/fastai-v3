de498a9ae46fbd3ea4682813d57f063c9cac5cf3
jamesr2323 <jarees@gmail.com>
2019-05-29 21:56:57 +0100

    Change to pressups

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=14XpRA3mG3ofPmJBPMV3wFQvqql6yRVMY'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['pressups', 'crunches']
 path = Path(__file__).parent
 
 app = Starlette()
